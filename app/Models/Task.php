af4826f12a464899e801afd75c7594c7a6222942
Eberth <the.jonkey@gmail.com>
2023-07-27 15:26:32 -0600

    CRUD of task ready

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,7 +9,7 @@ class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'done'];
+    protected $fillable = ['title', 'description', 'done', 'user_id'];
 
     public function user()
     {
---
+++ resources/views/livewire/task-component.blade.php
@@ -0,0 +1,30 @@
+<div>
+    <form wire:submit.prevent="save">
+        <input type="text" wire:model="task.title">
+
+        <textarea wire:model="task.description"></textarea>
+
+        <button type="submit">Save</button>
+    </form>
+
+    @if (session()->has('message'))
+        <h3>{{ session('message') }}</h3>
+    @endif
+
+    <input type="text" wire:model="title" />
+    <input type="text" wire:model="description" />
+    <button wire:click="createTask">Agregar tarea</button>
+
+    @forelse($tasks as $task)
+        <li>
+            <h3>{{ $task->title }}</h3>
+            <p>{{ $task->description }}</p>
+            <input type="checkbox" wire:click="done({{ $task->id }})">
+            <button wire:click="deleteTask({{ $task->id }})">Eliminar</button>
+            <button wire:click="updateTask({{ $task->id }})">Editar</button>
+        </li>
+    @empty
+        <h3>No hay tareas</h3>
+    @endforelse
+
+</div>
